621095d386b7ba72c0b36b63dc017168b5a16f85
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2023-03-01 23:33:46 +0100

    seeder error

---
+++ database/migrations/2023_03_01_191002_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda');
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
+            $table->time('Orario di partenza');
+            $table->time('Orario di arrivo');
+            $table->string('Codice Treno');
+            $table->integer('Numero di Carrozze');
+            $table->tinyInteger('In orario')->unsigned()->default(1);
+            $table->boolean('cancellato');
+            $table->timestamps('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $newTrain = new Train();
+        $newTrain->Azienda = '';
+        $newTrain->Stazione di partenza = '';
+        $newTrain->
+        $newTrain->
+        $newTrain->
+        $newTrain->
+        $newTrain->
+        $newTrain->
+        $newTrain->
+    }
+}
